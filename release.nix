79ae3a10ddfe86990af6bb09d8c7d55dd82b81e0
Alexander Gall <alexander.gall@gmail.com>
2021-03-12 10:32:34 +0100

    Fix URL in spec.json

--- release.nix
+++ release.nix
@@ -4,6 +4,6 @@ with import ./.;
 
 ## Have the CI build the data-plane and control-plane programs as well
 ## as the closure for binary deployments.
-release // {
-  inherit closure;
+{
+  inherit release closure;
 }
