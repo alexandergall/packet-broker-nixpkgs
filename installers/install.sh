5b54bb64bf55b7399cd0dda90e7c5317d543b2e3
Alexander Gall <alexander.gall@gmail.com>
2021-04-08 08:44:56 +0000

    Improve rlease-manager and release installer functionality

--- installers/install.sh
+++ installers/install.sh
@@ -1,19 +1,41 @@
 #!/bin/bash
 version=$(cat ./version)
 versionID=$(cat ./version.id)
-profile=$(cat ./profile)
+PROFILE=$(cat ./profile)
 kernelRelease=$(uname -r)
 
 PATH=/nix/var/nix/profiles/default/bin:@PATH@
+NIX_PATH=
 
 error () {
     echo "ERROR: $1"
     exit 1
 }
 
+declare -A gens gens_by_id
+
+current_gen=0
+if [ -h $PROFILE ]; then
+    for path in $PROFILE-*-link; do
+	[ -h $path ] || continue
+	short_path=$(basename $path)
+	gen=$(echo $short_path | sed -e 's/.*-\([0-9]*\)-link$/\1/')
+	[ $gen -gt $current_gen ] && current_gen=$gen
+	gens[$gen]=$short_path
+	id=$(cat $path/version.id)
+	gens_by_id[$id]=$gen
+  done
+fi
+
 echo "Installing packet broker release $version (Id: $versionID)"\
-     "for kernel $kernelRelease in $profile"
+     "for kernel $kernelRelease in $PROFILE"
 [ $(id -u) == 0 ] || error "Please run this command as root"
+
+if [ -n "${gens_by_id[$versionID]}" ]; then
+    echo "This release is already installed as generation #${gens_by_id[$versionID]}:"
+    $PROFILE/bin/release-manager --list-installed
+    exit 1
+fi
 
 [ -d $kernelRelease ] || error "Unsupported kernel"
 kernelIDs=$kernelRelease/*
@@ -42,27 +64,17 @@ done
 echo "Registering paths in DB"
 cat $closureInfo/registration | nix-store --load-db
 
-echo "Installing the service in $profile"
-current_gen () {
-    [ -e $profile ] || echo ""
-    echo $(readlink $profile | sed -e 's/.*-\([0-9]*\)-link$/\1/')
-}
-
-gen=$(current_gen)
-nix-env -p $profile -i -r $(cat $closureInfo/rootPaths)
+echo "Installing the service in $PROFILE"
+nix-env -p $PROFILE -i -r $(cat $closureInfo/rootPaths)
 echo
 echo "Installation completed"
-if [ -n "$gen" ]; then
-    if [ $(current_gen) != $gen ]; then
-	nix-env -p $profile --rollback
-	echo "Use \"release-manager --switch-to-generation $gen\" to switch to this release"
-    else
-	echo -n "The new release is identical to that "
-        echo -n "in the latest generation ($gen) of the profile, "
-	echo    "nothing changed"
-    fi
+if [ $current_gen -gt 0 ]; then
+    nix-env -p $PROFILE --rollback 2>/dev/null
+    echo "Use \"release-manager --switch-to-generation $((current_gen + 1))\" to switch to this release"
+else
+    echo "This is the first installation of the service."
+    echo "Use \"$PROFILE/bin/release-manager --activate\" to start."
 fi
 
-echo
 echo "Currently installed releases:"
-$profile/bin/release-manager --list-installed
+$PROFILE/bin/release-manager --list-installed
