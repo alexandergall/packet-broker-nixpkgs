f75d506956c75a7ab3e1dea2b333eca4a3a700e0
Alexander Gall <alexander.gall@gmail.com>
2021-08-09 14:05:11 +0200

    Cleanup release.nix
    Also, don't build standaloneInstaller if buildRelease is false.

--- release.nix
+++ release.nix
@@ -6,21 +6,24 @@ with import ./. {
   inherit onieUsers componentSuffix;
 };
 
-## The releases and releasesClosure derivations have the same
-## closure. releasesClosure is used by the Hydra post-build hook
-## to copy the closure to a separate binary cache.
-(if buildRelease then
-  {
-    inherit release releaseClosure;
-  }
-else
-  {}) //
-(if buildInstaller then
-  { inherit standaloneInstaller; }
-  // (if builtins.match "release-[^-]+" src.gitTag != null then
-       ## Build the ONIE installer for principal releases only
-       { inherit onieInstaller; }
-      else
-        {})
-else
-  {})
+let
+  optionalAttrs = cond: as: if cond then as else {};
+in
+(
+  optionalAttrs buildRelease
+    {
+      ## The releases and releasesClosure derivations have the same
+      ## closure. releasesClosure is used by the Hydra post-build hook
+      ## to copy the closure to a separate binary cache.
+      inherit release releaseClosure;
+    }
+) //
+(
+  optionalAttrs buildInstaller
+    (
+      (optionalAttrs buildRelease { inherit standaloneInstaller; }) //
+      (optionalAttrs (builtins.match "release-[^-]+" src.gitTag != null)
+        ## Build the ONIE installer for principal releases only
+        { inherit onieInstaller; })
+    )
+)
