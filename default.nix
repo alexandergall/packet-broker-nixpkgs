54ecad715e8868c6e68559d510885c71d83cf19f
Alexander Gall <alexander.gall@gmail.com>
2023-02-14 13:50:13 +0100

    Release 7 beta

--- default.nix
+++ default.nix
@@ -15,7 +15,7 @@ let
   ## Release version of the packet broker service.  The commit for the
   ## release is tagged with "release-<version>". See
   ## README.md#versioning for the full Git workflow.
-  version = "6";
+  version = "7";
   versionFile = pkgs.writeTextDir "version" "${version}:${gitTag}\n";
   nixProfile = "/nix/var/nix/profiles/packet-broker";
 
